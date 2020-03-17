d08b2f00e9d15f416278a311694e59404494fab3
Brian Nicholls <nichol88@gmail.com>
2020-03-17 15:24:06 -0400

    create google login controller action

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,11 +5,22 @@ class StaticController < ApplicationController
   end
 
   def signup
-
+    @user = User.new
   end
 
-  def login
+  def google_login
+    auth_hash = request.env['omniauth.auth']
+    user_info = auth_hash['info']
 
+    user = User.find_or_create_by email: user_info['email']
+    user.update(
+      fname: user_info['first_name'],
+      lname: user_info['last_name'],
+      password: SecureRandom.hex
+    )
+
+    session[:user_id] = user.id
+    redirect_to user_path(user)
   end
 
 
