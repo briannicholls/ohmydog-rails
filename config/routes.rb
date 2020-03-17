8ec9b9dd825a2b36dc8ce539e40b2f11161b5d92
Brian Nicholls <nichol88@gmail.com>
2020-03-17 15:19:32 -0400

    add routes: login, signup, logout, google login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   resources :users
   resources :pets
 
+  get 'login', to: 'sessions#new'
+  get 'signup', to: 'static#signup'
+  get 'logout', to: 'sessions#destroy'
+
+  get '/auth/google_oauth2/callback', to: 'static#google_login'
+
   root 'static#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
