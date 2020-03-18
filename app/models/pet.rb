8e80889068b1685e231d787fa12b636fcf88eebc
Brian Nicholls <nichol88@gmail.com>
2020-03-17 23:20:15 -0400

    add #name_with_owner method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,8 @@ class Pet < ApplicationRecord
   belongs_to :owner, required: false
   has_many :walks
   has_many :users, through: :walks
+
+  def name_with_owner
+    "#{name} (#{owner.name})" if owner
+  end
 end
