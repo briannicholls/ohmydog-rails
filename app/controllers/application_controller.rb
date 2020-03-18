e48e9b9a5112061f2865c238a77e4e1b9ed2fa37
Brian Nicholls <nichol88@gmail.com>
2020-03-17 23:18:17 -0400

    change #current_user find method to find_by

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   def current_user
-    User.find session[:user_id]
+    User.find_by id: session[:user_id]
   end
   helper_method :current_user
 
