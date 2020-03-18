16e7131b9aca4f9af002ae7fb11c44510493ca92
Brian Nicholls <nichol88@gmail.com>
2020-03-17 23:21:19 -0400

    add scope methods for walks, beautify pickup_window method

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -2,7 +2,11 @@ class Walk < ApplicationRecord
   belongs_to :pet
   has_one :walker, class_name: :user
 
+  scope :today, -> { where(window_start: DateTime.current.midnight) }
+  scope :completed, -> { where(completed?: true) }
+
   def pickup_window
-    "#{window_start} - #{window_end}"
+    "#{window_start.strftime('%l:%M%P')} - #{window_end.strftime('%l:%M%P')}"
   end
+
 end
