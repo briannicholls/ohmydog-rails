31de4b7953ec417e0f01a14c83ec63bc930769e7
Brian Nicholls <nichol88@gmail.com>
2020-03-20 18:21:10 -0400

    added sessions#create for regular logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by email: params[:email]
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to walks_path
+    else
+      flash.alert = "Incorrect email/password!"
+      render :new
+    end
   end
 
   def destroy
     session.delete :user_id
     redirect_to root_path
   end
+
 end
