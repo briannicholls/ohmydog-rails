618dfa2a916fba84d1100e22ca6543f813b10b28
Brian Nicholls <nichol88@gmail.com>
2020-03-20 16:32:13 -0400

    add custom helper edit_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def edit_button(model)
+    name = model.class.to_s
+    link_to("Edit #{name}", send("edit_#{name.downcase}_path", model), class: 'w3-button w3-bar w3-yellow')
+  end
 end
