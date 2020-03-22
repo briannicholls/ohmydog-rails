381e4bc054f5246ef991f3809e3643abe26aa995
Brian Nicholls <nichol88@gmail.com>
2020-03-21 22:27:38 -0400

    put seeds inside method to only call when desired

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,47 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def call
+  10.times.with_index do |i|
 
-10.times.with_index do |i|
+    Owner.create(
+      address: Faker::Address.street_address,
+      apartment_number: Faker::Address.secondary_address,
+      zip_code: Faker::Address.zip_code,
+      door_code: Faker::Address.building_number,
+      lockbox_code: Faker::Address.building_number,
+      entry_instructions: Faker::ChuckNorris.fact,
+      neighborhood: Faker::Address.community,
+      phone2: Faker::PhoneNumber.cell_phone,
+      phone: Faker::PhoneNumber.cell_phone,
+      fname: Faker::Name.first_name,
+      lname: Faker::Name.last_name,
+      email: Faker::Internet.email
+    )
+  end
 
-  Owner.create(
-    address: Faker::Address.street_address,
-    apartment_number: Faker::Address.secondary_address,
-    zip_code: Faker::Address.zip_code,
-    door_code: Faker::Address.building_number,
-    lockbox_code: Faker::Address.building_number,
-    entry_instructions: Faker::ChuckNorris.fact,
-    neighborhood: Faker::Address.community,
-    phone2: Faker::PhoneNumber.cell_phone,
-    phone: Faker::PhoneNumber.cell_phone,
-    fname: Faker::Name.first_name,
-    lname: Faker::Name.last_name,
-    email: Faker::Internet.email
-  )
+  Owner.all.each do |owner|
+    owner.pets.build(
+      name: Faker::Cannabis.strain,
+      breed: Faker::Creature::Dog.breed,
+      gender: Faker::Creature::Dog.gender,
+      appearance: Faker::Creature::Dog.meme_phrase,
+      notes: Faker::Creature::Dog.age,
+      temperament: Faker::Creature::Dog.sound,
+      birthday: Faker::Date.birthday(min_age: 0, max_age: 15).strftime('%Y-%m-%dT%H%M')
+    ).save
+  end
+
+  Pet.all.each do |pet|
+    date = DateTime.parse(Faker::Date.between(from: 1.days.ago, to: 5.days.from_now).to_s)
+    start_time = Time.parse(date.to_s) + rand(0..7200)
+    end_time = start_time + 3600
+
+    pet.walks.build(
+      date: date,
+      window_start: start_time,
+      window_end: end_time,
+      notes: Faker::Movies::Lebowski.quote,
+      walk_type: '30 min'
+    ).save
+  end
 end
 
-Owner.all.each do |owner|
-  owner.pets.build(
-    name: Faker::Cannabis.strain,
-    breed: Faker::Creature::Dog.breed,
-    gender: Faker::Creature::Dog.gender,
-    appearance: Faker::Creature::Dog.meme_phrase,
-    notes: Faker::Creature::Dog.age,
-    temperament: Faker::Creature::Dog.sound,
-    birthday: Faker::Date.birthday(min_age: 0, max_age: 15).strftime('%Y-%m-%dT%H%M')
-  ).save
-end
-
-Pet.all.each do |pet|
-  date = DateTime.parse(Faker::Date.between(from: 1.days.ago, to: 5.days.from_now).to_s)
-  start_time = Time.parse(date.to_s) + rand(0..7200)
-  end_time = start_time + 3600
-
-  pet.walks.build(
-    date: date,
-    window_start: start_time,
-    window_end: end_time,
-    notes: Faker::Movies::Lebowski.quote,
-    walk_type: '30 min'
-  ).save
-end
+#call()
