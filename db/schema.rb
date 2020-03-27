aee5031485b6e4289bb7688034af77861624709c
nichol88 <nichol88@gmail.com>
2020-03-26 22:13:17 -0400

    change db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_17_234650) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "owners", force: :cascade do |t|
     t.string "address"
     t.string "apartment_number"
