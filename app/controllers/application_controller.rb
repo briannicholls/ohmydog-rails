7fa3e1f42f2c1637562f62d34463ccc6f737b6b1
brian nicholls <nichol88@gmail.com>
2025-06-01 00:18:47 +0000

    add first and last name to devise sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :lname, :email, :password, :password_confirmation, :current_password])
+  end
 
   # Devise: Where to redirect users after sign in.
   def after_sign_in_path_for(resource_or_scope)
